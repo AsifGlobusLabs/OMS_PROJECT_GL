71fa691a3fbc8198de5f525e19a2479040123899
nawaid <iqbalnawaid15@gmail.com>
2024-02-05 15:17:57 +0530

    Worked on assignment api's

---
+++ server/routes/assignmentRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const assignmentController = require("../controllers/assignmentControllers");
+
+// get all user details
+router.get("/", assignmentController.getAllAssignments);
+
+// add user details
+router.post("/", assignmentController.addAssignment);
+
+// update user details
+router.patch("/update/:AssignmentID", assignmentController.updateAssignment);
+
+// delete user details
+router.delete("/delete/:AssignmentID", assignmentController.deleteAssignment);
+
+
+module.exports = router;
