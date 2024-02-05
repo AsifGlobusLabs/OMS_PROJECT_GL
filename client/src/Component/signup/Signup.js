7be3f958e9d0f60ba830efecbbebf927a4b4828e
miteshpr97 <miteshpradhan97@gmail.com>
2024-02-05 16:36:18 +0530

    singup form work

--- client/src/Component/signup/Signup.js
+++ client/src/Component/signup/Signup.js
@@ -1,13 +1,14 @@
-
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Avatar,
   Button,
   CssBaseline,
-  TextField,
-  FormControlLabel,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
   Checkbox,
-  Link,
+  FormControlLabel,
   Grid,
   Box,
   Typography,
@@ -19,29 +20,75 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Card from "@mui/material/Card"; // Import Card component
 import CardContent from "@mui/material/CardContent"; // Import CardContent component
 import HeaderSignUp from "./HeaderSignUp";
+import TextField from "@mui/material/TextField";
 
 const customTheme = createTheme({
   palette: {
     background: {
-      default: 'whitesmoke', // Change the default background color here
+      default: "whitesmoke", // Change the default background color here
     },
   },
 });
 
 export default function SignUp() {
+  const [data, setData] = useState([]);
+  const [formData, setFormData] = useState({
+    EmployeeID: "",
+    Username: "",
+    Password: "",
+    ConfirmPassword: "", // Include ConfirmPassword field in formData
+    acceptTerms: false,
+  });
+
+  console.log(data, "dkdhd");
+
+  const handleChange = (event) => {
+    const { name, value, checked, type } = event.target;
+    const newValue = type === "checkbox" ? checked : value;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: newValue,
+    }));
+  };
+
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const apiUrl = "http://localhost:3306/api/employee";
+        const response = await fetch(apiUrl, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        const result = await response.json();
+        setData(result.reverse()); // Reverse the order of fetched data
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+
+    // Check if the Password and ConfirmPassword fields match
+    if (formData.Password !== formData.ConfirmPassword) {
+      alert("Passwords do not match. Please re-enter.");
+      return; // Do not proceed with form submission
+    }
+    console.log(formData);
   };
 
   return (
     <ThemeProvider theme={customTheme}>
       <HeaderSignUp />
-      <Container component="main" maxWidth="sm" >
+      <Container component="main" maxWidth="sm">
         <CssBaseline />
         <Box
           sx={{
@@ -73,52 +120,81 @@ export default function SignUp() {
                 sx={{ mt: 3 }}
               >
                 <Grid container spacing={2}>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      autoComplete="given-name"
-                      name="firstName"
-                      required
-                      fullWidth
-                      id="firstName"
-                      label="First Name"
-                      autoFocus
-                    />
+                  <Grid item xs={6}>
+                    <FormControl fullWidth>
+                      <InputLabel id="employee-id-label">
+                        Employee ID
+                      </InputLabel>
+                      <Select
+                        labelId="employee-id-label"
+                        id="EmployeeID"
+                        name="EmployeeID"
+                        value={formData.EmployeeID}
+                        onChange={handleChange}
+                      >
+                        {data.map((item) => (
+                          <MenuItem key={item.id} value={item.id}>
+                            {item.EmployeeID} -- {item.FirstName}{" "}
+                            {item.LastName}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                    </FormControl>
                   </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      required
-                      fullWidth
-                      id="lastName"
-                      label="Last Name"
-                      name="lastName"
-                      autoComplete="family-name"
-                    />
+                  <Grid item xs={6}>
+                    <FormControl fullWidth>
+                      <InputLabel id="username-label">Username</InputLabel>
+                      <Select
+                        labelId="username-label"
+                        id="Username"
+                        name="Username"
+                        value={formData.Username}
+                        onChange={handleChange}
+                      >
+                        {data.map((item) => (
+                          <MenuItem key={item.id} value={item.username}>
+                            {item.username}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                    </FormControl>
                   </Grid>
                   <Grid item xs={12}>
                     <TextField
                       required
                       fullWidth
-                      id="email"
-                      label="Email Address"
-                      name="email"
-                      autoComplete="email"
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      required
-                      fullWidth
-                      name="password"
+                      name="Password"
                       label="Password"
                       type="password"
-                      id="password"
+                      id="Password"
                       autoComplete="new-password"
+                      value={formData.Password}
+                      onChange={handleChange}
                     />
                   </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      name="ConfirmPassword"
+                      label="Confirm Password"
+                      type="password"
+                      id="ConfirmPassword"
+                      autoComplete="new-password"
+                      value={formData.ConfirmPassword}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+
                   <Grid item xs={12}>
                     <FormControlLabel
                       control={
-                        <Checkbox value="allowExtraEmails" color="primary" />
+                        <Checkbox
+                          checked={formData.acceptTerms}
+                          color="primary"
+                          name="acceptTerms"
+                          onChange={handleChange}
+                        />
                       }
                       label="I accept all terms and conditions"
                     />
@@ -132,7 +208,6 @@ export default function SignUp() {
                 >
                   Sign Up
                 </Button>
-               
               </Box>
             </CardContent>
           </Card>
