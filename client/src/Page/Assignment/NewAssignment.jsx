854eb16811b8b72f1d20e1864ab69fad19cb03ca
miteshpr97 <miteshpradhan97@gmail.com>
2024-02-05 16:39:53 +0530

    assignment card

--- client/src/Page/Assignment/NewAssignment.jsx
+++ client/src/Page/Assignment/NewAssignment.jsx
@@ -3,31 +3,45 @@ import SideBar from "../../Component/SideBar";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import "./Assignment.css";
-import Card from "react-bootstrap/Card";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
 
 export default function NewAssignment() {
   return (
     <Box sx={{ display: "flex" }}>
       <SideBar />
       <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: "55px" }}>
-        <Typography variant="h5">New Assignment</Typography>
+        <Typography variant="h5">Assignment</Typography>
         <div className="assignment-container">
           <div className="assignment-card">
             {["Primary", "Success", "Danger"].map((variant) => (
               <Card
-                bg={variant.toLowerCase()}
                 key={variant}
-                text={variant.toLowerCase() === "light" ? "dark" : "white"}
-                style={{ width: "16rem", height:"10rem" }}
-                className="mb-2"
+                sx={{
+                  backgroundColor:
+                    variant.toLowerCase() === "primary"
+                      ? "#007bff"
+                      : variant.toLowerCase() === "success"
+                      ? "#28a745"
+                      : variant.toLowerCase() === "danger"
+                      ? "#dc3545"
+                      : "inherit",
+                  color: "white",
+                  width: "16rem",
+                  height: "10rem",
+                  marginBottom: "16px",
+                }}
               >
-                <Card.Header>Header</Card.Header>
-                <Card.Body>
-                  <Card.Title>{variant} Card Title </Card.Title>
-                  <Card.Text>
-                    Some quick example text to build 
-                  </Card.Text>
-                </Card.Body>
+                <CardHeader title="Header" />
+                <CardContent>
+                  <Typography variant="h6" component="div">
+                    {variant} Card Title
+                  </Typography>
+                  <Typography variant="body2">
+                    Some quick example text to build
+                  </Typography>
+                </CardContent>
               </Card>
             ))}
           </div>
