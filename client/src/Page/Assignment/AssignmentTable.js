5d098010ebcafbb2949fce4735827c478127dc00
Asif <asif.ziakps@gmail.com>
2024-02-06 17:28:30 +0530

    assignment table data

--- client/src/Page/Assignment/AssignmentTable.js
+++ client/src/Page/Assignment/AssignmentTable.js
@@ -5,6 +5,8 @@ import { Tab, Table, Tabs } from "react-bootstrap";
 const AssignmentTable = () => {
   const [tableData, setTableData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(25);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,6 +31,12 @@ const AssignmentTable = () => {
     fetchData();
   }, []);
 
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredData.slice(indexOfFirstItem, indexOfLastItem);
+ // Change page
+ const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
 
   return (
     <div className="assignment-table">
@@ -36,107 +44,134 @@ const AssignmentTable = () => {
         Assignment Data
       </Typography>
 
-<div className="p-2">
+<div className="p-2" >
       <Tabs
         defaultActiveKey="profile"
         id="uncontrolled-tab-example"
         className="mb-3 mt-2"
       >
-        <Tab eventKey="home" title="Pending">
+
+        {/* pending table data  */}
+        <Tab eventKey="home" title="Pending" style={{ maxHeight: "400px", overflowY: "auto", marginTop:"20px" }}>
+          <Table striped bordered hover size="sm" > 
+            <thead>
+              <tr>
+                <th>AssignmentID</th>
+                <th>EmployeeID</th>
+                <th>EmployeeID AssignTo</th>
+                <th>Assignment Description</th>
+                <th>Assign Date</th>
+                <th>Deadline Date</th>
+                <th>Assignment Status</th>
+                <th>Assignment Priority</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+            {currentItems.map((item) => (
+              <tr>
+                <td>{item.AssignmentID}</td>
+                <td>{item.EmployeeID}</td>
+                <td>{item.EmployeeID_AssignTo}</td>
+                <td>{item.Assignment_Description}</td>
+                <td>{item.AssignDate}</td>
+                <td>{item.DeadlineDate}</td>
+                <td>{item.AssignmentStatus}</td>
+                <td>{item.AssignmentPriority}</td>
+                <td>{item.Type}</td>
+              </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Tab>
+
+        {/* progress table data  */}
+
+        <Tab eventKey="profile" title="Progress" style={{ maxHeight: "400px", overflowY: "auto", marginTop:"20px" }}>
           <Table striped bordered hover size="sm"> 
             <thead>
               <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Username</th>
+                <th>AssignmentID</th>
+                <th>EmployeeID</th>
+                <th>EmployeeID AssignTo</th>
+                <th>Assignment Description</th>
+                <th>Assign Date</th>
+                <th>Deadline Date</th>
+                <th>Assignment Status</th>
+                <th>Assignment Priority</th>
+                <th>Type</th>
               </tr>
             </thead>
             <tbody>
+            {currentItems.map((item) => (
               <tr>
-                <td>1</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
+                <td>{item.AssignmentID}</td>
+                <td>{item.EmployeeID}</td>
+                <td>{item.EmployeeID_AssignTo}</td>
+                <td>{item.Assignment_Description}</td>
+                <td>{item.AssignDate}</td>
+                <td>{item.DeadlineDate}</td>
+                <td>{item.AssignmentStatus}</td>
+                <td>{item.AssignmentPriority}</td>
+                <td>{item.Type}</td>
               </tr>
-              <tr>
-                <td>2</td>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td colSpan={2}>Larry the Bird</td>
-                <td>@twitter</td>
-              </tr>
+              ))}
             </tbody>
           </Table>
         </Tab>
-        <Tab eventKey="profile" title="Progress">
-          <Table striped bordered hover size="sm">
+
+        {/* complete table data  */}
+        <Tab eventKey="contact" title="Complete" style={{ maxHeight: "400px", overflowY: "auto", marginTop:"20px" }}>
+        <Table striped bordered hover size="sm"> 
             <thead>
               <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Username</th>
+                <th>AssignmentID</th>
+                <th>EmployeeID</th>
+                <th>EmployeeID AssignTo</th>
+                <th>Assignment Description</th>
+                <th>Assign Date</th>
+                <th>Deadline Date</th>
+                <th>Assignment Status</th>
+                <th>Assignment Priority</th>
+                <th>Type</th>
               </tr>
             </thead>
             <tbody>
+            {currentItems.map((item) => (
               <tr>
-                <td>1</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
+                <td>{item.AssignmentID}</td>
+                <td>{item.EmployeeID}</td>
+                <td>{item.EmployeeID_AssignTo}</td>
+                <td>{item.Assignment_Description}</td>
+                <td>{item.AssignDate}</td>
+                <td>{item.DeadlineDate}</td>
+                <td>{item.AssignmentStatus}</td>
+                <td>{item.AssignmentPriority}</td>
+                <td>{item.Type}</td>
               </tr>
-              <tr>
-                <td>2</td>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td colSpan={2}>Larry the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </Table>
-        </Tab>
-        <Tab eventKey="contact" title="Complete" >
-          <Table striped bordered hover size="sm">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Username</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td colSpan={2}>Larry the Bird</td>
-                <td>@twitter</td>
-              </tr>
+              ))}
             </tbody>
           </Table>
         </Tab>
       </Tabs>
       </div>
+
+       {/* Pagination */}
+       <ul className="pagination">
+            {Array.from(
+              { length: Math.ceil(filteredData.length / itemsPerPage) },
+              (_, index) => (
+                <li key={index} className="page-item">
+                  <button
+                    onClick={() => paginate(index + 1)}
+                    className="page-link"
+                  >
+                    {index + 1}
+                  </button>
+                </li>
+              )
+            )}
+          </ul>
     </div>
   );
 };
