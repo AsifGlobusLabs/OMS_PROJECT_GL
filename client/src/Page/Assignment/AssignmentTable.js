3cddebc62197356eeeea91f5e91e43a2bf2f7bbb
Asif <asif.ziakps@gmail.com>
2024-02-06 17:01:40 +0530

    assignment form

--- client/src/Page/Assignment/AssignmentTable.js
+++ client/src/Page/Assignment/AssignmentTable.js
@@ -1,41 +1,144 @@
-import { Typography } from '@mui/material'
-import React from 'react'
-import { Table } from 'react-bootstrap'
+import { Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Tab, Table, Tabs } from "react-bootstrap";
 
 const AssignmentTable = () => {
-  return (
-    <div className='assignment-table'>
-        <Typography variant="h5" style={{fontWeight:"500"}}>Assignment Data</Typography> 
-        <Table striped bordered hover size="sm">
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Username</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>1</td>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-      </tr>
-      <tr>
-        <td>2</td>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-      </tr>
-      <tr>
-        <td>3</td>
-        <td colSpan={2}>Larry the Bird</td>
-        <td>@twitter</td>
-      </tr>
-    </tbody>
-  </Table></div>
-  )
-}
+  const [tableData, setTableData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
 
-export default AssignmentTable
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const apiUrl = "http://localhost:3306/api/assignmentDetails";
+        const response = await fetch(apiUrl, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        const result = await response.json();
+        const reversedData = result.reverse();
+        setTableData(reversedData);
+        setFilteredData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+  return (
+    <div className="assignment-table">
+      <Typography variant="h5" style={{ fontWeight: "500" }}>
+        Assignment Data
+      </Typography>
+
+<div className="p-2">
+      <Tabs
+        defaultActiveKey="profile"
+        id="uncontrolled-tab-example"
+        className="mb-3 mt-2"
+      >
+        <Tab eventKey="home" title="Pending">
+          <Table striped bordered hover size="sm"> 
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td colSpan={2}>Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Tab>
+        <Tab eventKey="profile" title="Progress">
+          <Table striped bordered hover size="sm">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td colSpan={2}>Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Tab>
+        <Tab eventKey="contact" title="Complete" >
+          <Table striped bordered hover size="sm">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td colSpan={2}>Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Tab>
+      </Tabs>
+      </div>
+    </div>
+  );
+};
+
+export default AssignmentTable;
